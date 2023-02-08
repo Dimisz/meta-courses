d198eb71797c9df8468df6241dbd8dadeadf0de9
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-08 20:51:38 +0800

    Completed projects section

--- react/portfolio/src/components/Card.js
+++ react/portfolio/src/components/Card.js
@@ -7,7 +7,24 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return(
+    <VStack backgroundColor='white' borderRadius='1em'>
+      <Image src={imageSrc} alt={description} borderRadius='1em'/>
+      <Heading as='h3' size='lg' textColor='black' style={{ marginRight: 'auto', marginLeft: '.5em' }}>
+        {title}
+      </Heading>
+      <Text fontSize='lg' textAlign='justify' textColor='black' style={{ paddingLeft: '1em', paddingRight: '1em' }}>
+        {description}
+      </Text>
+
+      <Text fontSize='lg' textColor='black' style={{ marginRight: 'auto', marginLeft: '1em', marginBottom: '1em', fontWeight:'bold' }}>
+        See more <FontAwesomeIcon icon={faArrowRight} size='1x' />
+      </Text>
+      
+      
+      
+    </VStack>
+  );
 };
 
 export default Card;
