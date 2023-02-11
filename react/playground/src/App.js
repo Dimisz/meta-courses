d086a2fea123b9bd5ddb9281bd0f564642d8108f
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-11 19:48:00 +0800

    Housekeeping

--- react/playground/src/App.js
+++ react/playground/src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+// import Fetch from './lectures/Fetch';
+// import UseRef from './lectures/UseRef';
+// import UseReducer from './lectures/UseReducer';
+// function App() {
+//   return (
+//     <UseReducer/>
+//   );
+// }
 
-import Fetch from './lectures/Fetch';
-import UseRef from './lectures/UseRef';
-import UseReducer from './lectures/UseReducer';
-function App() {
-  return (
-    <UseReducer/>
-  );
+
+const Button = ({ children, ...rest }) => (
+  <button onClick={() => console.log("ButtonClick")} {...rest}>
+    {children}
+  </button>
+);
+
+const withClick = (Component) => {
+  const handleClick = () => {
+    console.log("WithClick");
+  };
+
+  return(props) => {
+    return<Component {...props} onClick={handleClick} />;
+  };
+};
+
+const MyButton = withClick(Button);
+
+export default function App() {
+  return <MyButton onClick={() => console.log("AppClick")}>Submit</MyButton>;
 }
-
-export default App;
+// export default App;
