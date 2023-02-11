acddad1c80d0296a5dee21520a75d7a179b223f6
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-11 12:15:29 +0800

    Added a scrollview component

--- react-native/SampleProject/components/WelcomeScreen.js
+++ react-native/SampleProject/components/WelcomeScreen.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
 
 const WelcomeScreen = () => {
+  const menuItemsToDisplay = [
+    'item 1 \n item2 \n item 3 \n item4 \nitem 5 \n item6 \nitem 7 \n item8 \n item 9 \n item10 \nitem 11 \n item12 \nitem 13 \n item14 \nitem 15 \n item16 \nitem 17 \n item19 \n'
+  ];
+
   return(
     <View style={styles.container}>
       <Text style={styles.head}>
@@ -10,6 +14,14 @@ const WelcomeScreen = () => {
       <Text style={styles.body}>
       Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!
       </Text>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.menuHeaderText}>
+          Little Lemon Menu
+        </Text>
+        <Text style={styles.menuItemsText}>
+          {menuItemsToDisplay[0]}
+        </Text>
+      </ScrollView>
     </View>
   );
 }
@@ -30,6 +42,20 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     color: '#EDEFEE',
     textAlign: 'center',
+  },
+  scrollView: {
+    paddingHorizontal: 40,
+    paddingVertical: 40,
+    backgroundColor: '#495E57'
+  },
+  menuHeaderText: {
+    color: '#F4CE14',
+    fontSize: 42,
+    flexWrap: 'wrap'
+  },
+  menuItemsText: {
+    color: '#F4CE14',
+    fontSize: 42
   }
 });
 
