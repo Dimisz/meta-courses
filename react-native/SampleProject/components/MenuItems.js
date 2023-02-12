48d924128e6587db89525962d1287ae8ad4bc604
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-12 10:49:08 +0800

    Used SectionList instead of FlatList

--- react-native/SampleProject/components/MenuItems.js
+++ react-native/SampleProject/components/MenuItems.js
@@ -1,86 +1,107 @@
 import React from 'react';
 
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-
-const green = "#495e57";
-const yellow = "#f4ce14";
+import { View, Text, StyleSheet, SectionList } from 'react-native';
 
 const menuItemsToDisplay = [
-  { name: 'Hummus', price: '$5.00', id: '1A' },
-  { name: 'Moutabal', price: '$5.00', id: '2B' },
-  { name: 'Falafel', price: '$7.50', id: '3C' },
-  { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-  { name: 'Kofta', price: '$5.00', id: '5E' },
-  { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-  { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-  { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-  { name: 'Kofta Burger', price: '$11.00', id: '9I' },
-  { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-  { name: 'Fries', price: '$3.00', id: '11K' },
-  { name: 'Buttered Rice', price: '$3.00', id: '12L' },
-  { name: 'Bread Sticks', price: '$3.00', id: '13M' },
-  { name: 'Pita Pocket', price: '$3.00', id: '14N' },
-  { name: 'Lentil Soup', price: '$3.75', id: '15O' },
-  { name: 'Greek Salad', price: '$6.00', id: '16Q' },
-  { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-  { name: 'Baklava', price: '$3.00', id: '18S' },
-  { name: 'Tartufo', price: '$3.00', id: '19T' },
-  { name: 'Tiramisu', price: '$5.00', id: '20U' },
-  { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+  {
+    title: 'Appetizers',
+    data: [
+      'Hummus',
+      'Moutabal',
+      'Falafel',
+      'Marinated Olives',
+      'Kofta',
+      'Eggplant Salad',
+    ],
+  },
+  {
+    title: 'Main Dishes',
+    data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+  },
+  {
+    title: 'Sides',
+    data: [
+      'Fries',
+      'Buttered Rice',
+      'Bread Sticks',
+      'Pita Pocket',
+      'Lentil Soup',
+      'Greek Salad',
+      'Rice Pilaf',
+    ],
+  },
+  {
+    title: 'Desserts',
+    data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+  },
 ];
 
-const ListHeader = () => <Text style={menuStyles.headerText}>Menu: </Text>;
+const Item = ({ name, price }) => (
+  <View style={menuStyles.innerContainer}>
+    <Text style={menuStyles.itemText}>{name}</Text>
+    <Text style={menuStyles.itemText}>{price}</Text>
+  </View>
+);
 
-const Separator = () => <View style={menuStyles.separator} />
+const Separator = () => <View style={menuStyles.separator} />;
 
-const Item = ({name, price}) => {
-  return(
-    <View style={menuStyles.innerContainer}>
-      <Text style={menuStyles.itemText}>{name}</Text>
-      <Text style={menuStyles.itemText}>{price}</Text>
-    </View>
-  )
-}
+const Footer = () => (
+  <Text style={menuStyles.footerText}>
+    All Rights Reserved by Little Lemon 2022
+  </Text>
+);
 
 const MenuItems = () => {
-  const renderItem = ({item}) => <Item name={item.name} price={item.price} />
-  return(
+  const renderItem = ({ item }) => <Item name={item} />;
+
+  const renderSectionHeader = ({ section: { title } }) => (
+    <Text style={menuStyles.sectionHeader}>{title} </Text>
+  );
+
+  return (
     <View style={menuStyles.container}>
-      <Text style={menuStyles.menuHeaderText}>
-        Little Lemon Menu
-      </Text>
-      <FlatList 
-        data={menuItemsToDisplay} 
-        keyExtractor={item => item.id} 
+      <SectionList
+        keyExtractor={(item, index) => item + index}
+        sections={menuItemsToDisplay}
         renderItem={renderItem}
-        ItemSeparatorComponent={Separator}
-        ListHeaderComponent={ListHeader}
-      />
+        renderSectionHeader={renderSectionHeader}
+        ListFooterComponent={Footer}
+        ItemSeparatorComponent={Separator}></SectionList>
     </View>
   );
-}
+};
 
+// Add styles to the component
 const menuStyles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.95,
   },
   innerContainer: {
     paddingHorizontal: 40,
     paddingVertical: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    backgroundColor: '#333333',
+  },
+  sectionHeader: {
+    backgroundColor: '#fbdabb',
+    color: '#333333',
+    fontSize: 34,
+    flexWrap: 'wrap',
+    textAlign: 'center',
   },
   itemText: {
     color: '#F4CE14',
-    fontSize: 20,
+    fontSize: 32,
   },
   separator: {
     borderBottomWidth: 1,
-    borderColor: '#edefee'
+    borderColor: '#EDEFEE',
   },
-  headerText: {
-    color: '#edefee',
+  footerText: {
+    color: '#EDEFEE',
     fontSize: 20,
-  }
+    flexWrap: 'wrap',
+    textAlign: 'center',
+  },
 });
+
 export default MenuItems;
