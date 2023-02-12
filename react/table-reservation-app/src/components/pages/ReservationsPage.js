200f8d9372d6c4b728362d97c90458fc30dd24f4
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-12 17:11:37 +0800

    Set up and styles the Reservation form

--- react/table-reservation-app/src/components/pages/ReservationsPage.js
+++ react/table-reservation-app/src/components/pages/ReservationsPage.js
@@ -1,8 +1,10 @@
 import Header from "../Header";
+import ReservationForm from "../reservation-form/ReservationForm";
 const ReservationsPage = () => {
   return(
     <main>
       <Header title='Reservations Page' />
+      <ReservationForm availableTimes={[1, 2, 3, 4]}/>
     </main>
   );
 }
