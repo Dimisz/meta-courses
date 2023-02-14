04916356dddd8470d0c9f63c2ec4a35b10eee99f
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-14 11:44:07 +0800

    Using loops with collections

---
+++ ios/swift-fundamentals/loopsWithCollections.playground/Contents.swift
@@ -0,0 +1,11 @@
+let names = ["Harry", "John", "Petere"]
+
+for aName in names {
+    print("Hello \(aName)!")
+}
+
+
+let vehicle = ["Unicycle": 1, "Bicycle": 2, "Car": 4]
+for (vehicleName, wheelCount) in vehicle {
+    print("\(vehicleName) has \(wheelCount) wheels")
+}
