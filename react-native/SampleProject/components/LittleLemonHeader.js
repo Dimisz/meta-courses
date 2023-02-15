51de8551dd2b658f7b1bd54d3c809e8efb592d1a
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-15 12:59:22 +0800

    First SwiftUI layout

--- react-native/SampleProject/components/LittleLemonHeader.js
+++ react-native/SampleProject/components/LittleLemonHeader.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Image} from 'react-native';
 
 const LittleLemonHeader = () => {
   return(
