f8635f0c79074b046f3ae39a16a0b3203c2211c0
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-15 19:06:50 +0800

    TextField with its methods

--- ios/swiftUI/Little Lemon Demo.swiftpm/ContentView.swift
+++ ios/swiftUI/Little Lemon Demo.swiftpm/ContentView.swift
@@ -1,9 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputValue: String = ""
+    
     var body: some View {
         VStack {
-            Text("Little Lemon")
+            Text("Little Lemon Restaurant is a family")
+                .fixedSize(horizontal: true, vertical: false)
+                .font(.title)
+                .foregroundColor(.gray)
+                .background(Color.black)
+                .padding()
+                .background(Color.gray)
+            Label("Lightning", systemImage: "bolt.fill")
         }
     }
 }
