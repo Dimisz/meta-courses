72c6da9340eb33eeaf5e03d3d8e5bf6ce12e6f15
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-21 10:06:04 +0800

    Completed the basic book app from Paul Hudson YouTube channel as extra on core data

--- ios/dataInIos/coreData/Bookworm/Bookworm/ContentView.swift
+++ ios/dataInIos/coreData/Bookworm/Bookworm/ContentView.swift
@@ -36,9 +36,14 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading){
+                    EditButton()
+                }
+                
                 ToolbarItem(placement: .navigationBarTrailing){
                     Button {
                         showingAddScreen.toggle()
@@ -51,6 +56,14 @@ struct ContentView: View {
                 AddBookView()
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet){
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        try? moc.save()
     }
 }
 
--- ios/dataInIos/coreData/Bookworm/Bookworm/DetailView.swift
+++ ios/dataInIos/coreData/Bookworm/Bookworm/DetailView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct DetailView: View {
     let book: Book
+    
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
+    
     var body: some View {
         ScrollView{
             ZStack(alignment: .bottomTrailing){
@@ -36,6 +41,26 @@ struct DetailView: View {
         }
         .navigationTitle(book.title ?? "Unknown book")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete book?", isPresented: $showingDeleteAlert){
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel){}
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
+    }
+    
+    func deleteBook(){
+        moc.delete(book)
+        
+        try? moc.save()
+        dismiss()
     }
 }
 
